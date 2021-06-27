927715ae16fb6a38a8ce615a2d155f802ef77d8c
xmichel <xavier.michel@neo9.fr>
2021-06-27 15:02:57 +0200

    watcher exception: Add retry on resource version too old exception

--- src/main/java/io/neo9/gatekeeper/controllers/controllers/IngressController.java
+++ src/main/java/io/neo9/gatekeeper/controllers/controllers/IngressController.java
@@ -7,9 +7,10 @@ import io.fabric8.kubernetes.api.model.networking.v1beta1.Ingress;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.Watch;
 import io.fabric8.kubernetes.client.Watcher;
-import io.fabric8.kubernetes.client.WatcherException;
 import io.neo9.gatekeeper.exceptions.VisitorGroupNotFoundException;
 import io.neo9.gatekeeper.services.VisitorGroupIngressReconciler;
+import io.neo9.gatekeeper.utils.RetryContext;
+import io.neo9.gatekeeper.utils.RetryableWatcher;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.stereotype.Component;
@@ -25,6 +26,8 @@ public class IngressController {
 
 	private final VisitorGroupIngressReconciler visitorGroupIngressReconciler;
 
+	private final RetryContext retryContext = new RetryContext();
+
 	private Watch ingressWatch;
 
 	public IngressController(KubernetesClient kubernetesClient, VisitorGroupIngressReconciler visitorGroupIngressReconciler) {
@@ -33,47 +36,42 @@ public class IngressController {
 	}
 
 	@PostConstruct
-	public void startWatchIngressChanges() {
+	public void startWatch() {
 		log.info("starting watch loop on ingress");
-		ingressWatch = kubernetesClient.network().ingresses().inAnyNamespace().withLabel(MUTABLE_LABEL_KEY, MUTABLE_LABEL_VALUE).watch(new Watcher<>() {
-			@Override
-			public boolean reconnecting() {
-				return true;
-			}
-
-			@Override
-			public void eventReceived(Action action, Ingress ingress) {
-				switch (action) {
-					case ADDED:
-					case MODIFIED:
-						String ingressName = ingress.getMetadata().getName();
-						log.info("update event detected for ingress : {}", ingressName);
-						try {
-							visitorGroupIngressReconciler.reconcile(ingress);
-						}
-						catch (VisitorGroupNotFoundException e) {
-							log.error("panic: could not resolve visitorGroup {} for ingress {}", e.getVisitorGroupName(), ingressName, e);
-						}
-						break;
-					default:
-						// do nothing on ingress deletion
-				}
-			}
-
-			@Override
-			public void onClose() {
-				Watcher.super.onClose();
-			}
-
-			@Override
-			public void onClose(WatcherException cause) {
-				log.warn("ingress watch was closed with error", cause);
-			}
-		});
+		watchIngresses();
 	}
 
 	@PreDestroy
-	public void stopWatchIngressChanges() {
+	public void stopWatch() {
+		log.info("closing watch loop on ingress");
 		ingressWatch.close();
+		retryContext.shutdown();
 	}
+
+	private void watchIngresses() {
+		Watcher<Ingress> watcher = new RetryableWatcher<>(retryContext, Ingress.class.getSimpleName(), this::watchIngresses, (action, ingress) -> {
+			switch (action) {
+				case ADDED:
+				case MODIFIED:
+					String ingressName = ingress.getMetadata().getName();
+					log.info("update event detected for ingress : {}", ingressName);
+					try {
+						visitorGroupIngressReconciler.reconcile(ingress);
+					}
+					catch (VisitorGroupNotFoundException e) {
+						log.error("panic: could not resolve visitorGroup {} for ingress {}", e.getVisitorGroupName(), ingressName, e);
+					}
+					break;
+				default:
+					// do nothing on ingress deletion
+			}
+			return null;
+		});
+
+		ingressWatch = kubernetesClient.network().ingresses()
+				.inAnyNamespace()
+				.withLabel(MUTABLE_LABEL_KEY, MUTABLE_LABEL_VALUE)
+				.watch(watcher);
+	}
+
 }
---
+++ src/main/java/io/neo9/gatekeeper/utils/RetryableWatcher.java
@@ -0,0 +1,42 @@
+package io.neo9.gatekeeper.utils;
+
+import java.util.function.BiFunction;
+
+import io.fabric8.kubernetes.client.Watcher;
+import io.fabric8.kubernetes.client.WatcherException;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class RetryableWatcher<T> implements Watcher<T> {
+
+	private final String retryKey;
+
+	private final RetryContext retryContext;
+
+	private final Runnable reconnect;
+
+	private final BiFunction<Action, T, Void> onEventReceived;
+
+	public RetryableWatcher(RetryContext retryContext, String retryKey, Runnable reconnect, BiFunction<Action, T, Void> onEventReceived) {
+		this.retryContext = retryContext;
+		this.retryKey = retryKey;
+		this.reconnect = reconnect;
+		this.onEventReceived = onEventReceived;
+	}
+
+	@Override
+	public void eventReceived(final Action action, final T resource) {
+		retryContext.reset(retryKey);
+		onEventReceived.apply(action, resource);
+	}
+
+	@Override
+	public void onClose(final WatcherException cause) {
+		if (cause != null) {
+			log.warn("Watch connection is closed. Try to re-watch {}", retryKey, cause);
+			retryContext.retry(retryKey, reconnect);
+		}
+	}
+
+}
+
