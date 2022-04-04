057742e5f75e752901a0d276994b9e5649883ba3
xaviermichel <xavier.michel@neo9.fr>
2022-04-04 18:06:27 +0200

    tests: split integrations tests

---
+++ scripts/run-integration-test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -x
+
+if [ ! -z "$1" ]; then
+    kubeContextArgs="--context=$1"
+fi
+
+source ./integration-tests/utils.sh
+
+for f in $(ls ../example-conf/); do
+    kubectl ${kubeContextArgs} apply -f ../example-conf/$f
+done
+
+sleep 10
+
+. ./integration-tests/it-http-filtering.sh "http-filtering"
+
+. ./integration-tests/it-istio-sidecar.sh "istio-sidecar"
+
+. ./integration-tests/it-expoer.sh "ingress-exposer"
+
+. ./integration-tests/it-tcp-filtering.sh "tcp-filtering"
